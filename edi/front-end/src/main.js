ecdb8167481cbb77dd4265f2f7cd4763a7c7ecd8
D0ctrine <hiseok3736@naver.com>
2023-02-24 11:44:03 +0900

    vue version upgrade and reset

--- edi/front-end/src/main.js
+++ edi/front-end/src/main.js
@@ -1,39 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
-import Vuelidate from 'vuelidate'
-import { library as faLibrary } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faSearch, faPlus, faFileAlt, faKey, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import vuetify from './plugins/vuetify'
-import Codemirror from 'vue-codemirror'
-import 'codemirror/lib/codemirror.css'
 
-// Bootstrap axios
-axios.defaults.baseURL = '/api'
-axios.defaults.headers.common.Accept = 'application/json'
-axios.interceptors.response.use(
-  response => response,
-  (error) => {
-    return Promise.reject(error)
-  }
-)
-
-// Enable Vuelidate
-Vue.use(Vuelidate)
-Vue.use(Codemirror)
-
-// Set up FontAwesome
-faLibrary.add(faHome, faSearch, faPlus, faFileAlt, faUser, faKey, faEnvelope)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.config.productionTip = false
-
-new Vue({
-  vuetify,
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
